eee1cbd8f0c1ab6ea1299c5fb2937c1a44745daf
Bradley David Bergeron <bradbergeron@gmail.com>
2014-07-29 11:18:09 -0400

    Documentation! :pencil:

--- BDBSpinKitRefreshControl/BDBSpinKitRefreshControl.h
+++ BDBSpinKitRefreshControl/BDBSpinKitRefreshControl.h
@@ -26,30 +26,113 @@
 
 
 #pragma mark -
+/**
+ *  Delegate for BDBSpinKitRefreshControl
+ *
+ *  @since 1.0.1
+ */
 @protocol BDBSpinKitRefreshControlDelegate
 <NSObject>
 
 @optional
+/**
+ *  Delegate method called once the refresh control has been displayed.
+ *
+ *  @since 1.0.1
+ */
 - (void)didShowRefreshControl;
+
+/**
+ *  Delegate method called once the refresh control has been hidden.
+ *
+ *  @since 1.0.1
+ */
 - (void)didHideRefreshControl;
 
 @end
 
+
+/**
+ *  BDBSpinKitRefreshControl is a drop-in replacement for UIRefreshControl that utilizes
+ *  the fantastic SpinKit (https://github.com/tobiasahlin/SpinKit) progress indicators.
+ *
+ *  @since 1.0.0
+ */
 #pragma mark -
 @interface BDBSpinKitRefreshControl : UIRefreshControl
 
 #pragma mark Properties
+/**
+ *  ---------------------------------------------------------------------------------------
+ *  @name Properties
+ *  ---------------------------------------------------------------------------------------
+ */
+
+/**
+ *  Delegate that implements methods described in BDBSpinKitRefreshControlDelegate.
+ *
+ *  @since 1.0.1
+ */
 @property (nonatomic, weak) id <BDBSpinKitRefreshControlDelegate> delegate;
 
+/**
+ *  The underlying RTSpinKitView that replaces the default UIActivityIndicator.
+ *
+ *  @since 1.0.0
+ */
 @property (nonatomic, readonly) RTSpinKitView *spinner;
 
+/**
+ *  Tint color of the spinner.
+ *
+ *  @discussion This property overrides the default implemetation of tintColor in 
+ *              UIRefreshControl in order to prevent the default UIActivityIndicator from
+ *              becoming visible when changing the tint color.
+ *
+ *  @since 1.0.1
+ */
 @property (nonatomic) UIColor *tintColor;
+
+/**
+ *  When set to YES, the underlying RTSpinKitView will change color instantaneously.
+ *  Otherwise, any color change will take effect the next time the spinner is shown.
+ *  Default value is NO.
+ *
+ *  @since 1.0.1
+ */
 @property (nonatomic, assign) BOOL shouldChangeColorInstantly;
 
+
 #pragma mark Initialization
+/**
+ *  ---------------------------------------------------------------------------------------
+ *  @name Initialization
+ *  ---------------------------------------------------------------------------------------
+ */
+
+/**
+ *  Initialize a new BDBSpinKitRefreshControl instance with the specified style and color.
+ *
+ *  @param style RTSpinKitViewStyle that the refresh control should use.
+ *  @param color Color applied to the underlying RTSpinKitView.
+ *
+ *  @return New BDBSpinKitRefreshControl instance.
+ *
+ *  @since 1.0.0
+ */
 + (instancetype)refreshControlWithStyle:(RTSpinKitViewStyle)style
                                   color:(UIColor *)color;
 
+/**
+ *  Initialize a new BDBSpinKitRefreshControl instance with the specified style and color.
+ *
+ *  @param style RTSpinKitViewStyle that the refresh control should use.
+ *  @param color Color applied to the underlying RTSpinKitView.
+ *
+ *  @return New BDBSpinKitRefreshControl instance.
+ *
+ *  @since 1.0.0
+ */
 - (instancetype)initWithStyle:(RTSpinKitViewStyle)style
                         color:(UIColor *)color;
 
